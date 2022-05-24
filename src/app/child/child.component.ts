103cb68ccf1926415bf452d3a85f677ac5f51a3e
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-24 14:36:16 +0530

    Learn send data child to parent component

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css'],
+})
+export class ChildComponent implements OnInit {
+  @Input() title: string | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
